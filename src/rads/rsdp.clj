105565e952d95070af1d0a6a8fc6bfaf5f2fae06
Radford Smith <radsmith@gmail.com>
2017-03-19 00:04:01 -0700

    Add perfect links

--- src/rads/rsdp.clj
+++ src/rads/rsdp.clj
@@ -16,7 +16,9 @@
   (util/new-async-process
     (assoc opts :handler (fair-loss-link-handler))))
 
-(defn- stubborn-link-handler [sent delta]
+(def delta 3000)
+
+(defn- stubborn-link-handler [sent]
   (fn [{sl :pid {fll :pid} :fll} event trigger]
     (match [event]
       [[sl :init]] (do
@@ -34,25 +36,43 @@
 
 (defn new-stubborn-link [opts]
   (util/new-async-process
-    (assoc opts :handler (stubborn-link-handler (atom nil) 1000))))
+    (assoc opts :handler (stubborn-link-handler (atom nil)))))
+
+(defn- perfect-link-handler [delivered]
+  (fn [{pl :pid {sl :pid} :sl} event trigger]
+    (match [event]
+      [[pl :init]] (reset! delivered #{})
+      [[pl :send q m]] (trigger [sl :send q m])
+      [[sl :deliver p m]] (when-not (@delivered m)
+                            (swap! delivered conj m)
+                            (trigger [pl :deliver p m]))
+      :else nil)))
+
+(defn new-perfect-link [opts]
+  (util/new-async-process
+    (assoc opts :handler (perfect-link-handler (atom nil)))))
 
 (defn new-channels []
   (let [trigger-chan (async/chan 100)
         events-chan (async/chan 100)
         events-mult (async/mult events-chan)
         fll-events (async/chan 100)
-        sl-events (async/chan 100)]
+        sl-events (async/chan 100)
+        pl-events (async/chan 100)]
     (async/pipe trigger-chan events-chan)
     (async/tap util/timeouts-mult events-chan)
     (async/tap events-mult fll-events)
     (async/tap events-mult sl-events)
+    (async/tap events-mult pl-events)
     {:trigger-chan trigger-chan 
      :events-chan events-chan
      :events-mult events-mult
      :fll-events fll-events
-     :sl-events sl-events}))
+     :sl-events sl-events
+     :pl-events pl-events}))
 
-(defn new-system [{:keys [trigger-chan fll-events sl-events] :as channels}]
+(defn new-system [{:keys [trigger-chan fll-events sl-events pl-events]
+                   :as channels}]
   (-> (component/system-map
         :fll (new-fair-loss-link
                {:pid "fll"
@@ -61,9 +81,14 @@
         :sl (new-stubborn-link
               {:pid "sl"
                :events-chan sl-events
+               :trigger-chan trigger-chan})
+        :pl (new-perfect-link
+              {:pid "pl"
+               :events-chan pl-events
                :trigger-chan trigger-chan}))
       (component/system-using
-        {:sl [:fll]})))
+        {:sl [:fll]
+         :pl [:sl]})))
 
 (comment
   (require '[rads.rsdp.util :as util] :reload)
