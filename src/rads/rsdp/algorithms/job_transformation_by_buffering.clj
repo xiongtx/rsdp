25fc9964d4fe0b7365cfaffe4c53842225862bbf
Radford Smith <radsmith@gmail.com>
2017-03-26 15:16:22 -0700

    Add implementation for 1.3

--- src/rads/rsdp/algorithms/job_transformation_by_buffering.clj
+++ src/rads/rsdp/algorithms/job_transformation_by_buffering.clj
@@ -16,3 +16,25 @@
     - TH1: Guaranteed response: Every submitted job is eventually confirmed or its
            transformation fails.
     - TH2: Soundness: A submitted job whose transformation fails is not processed.")
+
+(defn advance [state event {:keys [th jh M] :as config}]
+  (let [{:keys [::top ::bottom ::handling ::buffer]} state]
+    (match [event]
+      [[th :init]] {::top 1
+                    ::bottom 1
+                    ::handling false
+                    ::buffer (into [] (take M (repeat nil)))}
+      [[th :submit job]] (if (= top (+ bottom M))
+                           (assoc state :trigger [[th :error job]])
+                           (-> state
+                               (assoc-in [::buffer (mod top (inc M))] job)
+                               (update ::top inc)
+                               (assoc :trigger [[th :confirm job]])))
+      [:upon] (when (and (< bottom top) (not handling))
+                (let [job (get buffer (mod bottom (inc M)))]
+                  (-> state
+                      (update ::bottom inc)
+                      (assoc ::handling true)
+                      (assoc ::trigger [[jh :submit job]]))))
+      [[jh :confirm job]] (assoc state ::handling false)
+      :else nil)))
